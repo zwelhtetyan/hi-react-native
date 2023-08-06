a6330de294cc3ecf14c96dd40ea67a3ebca916d6
zwelhtetyan <zwelhtetyan2001@gmail.com>
2023-08-06 21:56:29 +0630

    refactor(code): split component

--- App.tsx
+++ App.tsx
@@ -4,13 +4,13 @@ import {
   Button,
   Image,
   ScrollView,
-  StyleSheet,
   Text,
   TextInput,
   View,
 } from "react-native";
 import type { Todo as TodoType } from "./types/todo";
 import { Todo } from "./utils/todo";
+import { styles } from "./style";
 
 export default function App() {
   const [todos, setTodos] = useState<TodoType[]>([]);
@@ -34,7 +34,7 @@ export default function App() {
     <View style={styles.container}>
       {/* header */}
       <View style={styles.header}>
-        <Text style={styles.logoText}>📝NOTE</Text>
+        <Text style={styles.logoText}>🗒️ZNOTE</Text>
         <Image style={styles.logo} source={require("./assets/favicon.png")} />
       </View>
 
@@ -62,64 +62,3 @@ export default function App() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    paddingTop: 70,
-    paddingHorizontal: 20,
-    paddingBottom: 20,
-  },
-  header: {
-    flexDirection: "row",
-    width: "100%",
-    alignItems: "center",
-    justifyContent: "space-between",
-  },
-  logo: {
-    width: 35,
-    height: 35,
-  },
-  logoText: {
-    fontSize: 20,
-    fontWeight: "700",
-  },
-  inputContainer: {
-    width: "100%",
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    marginTop: 30,
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: "#cbd5e1",
-    height: 45,
-    padding: 10,
-    borderRadius: 5,
-    flex: 1,
-    marginRight: 10,
-  },
-  addBtn: {
-    backgroundColor: "#2563eb",
-    height: 45,
-    justifyContent: "center",
-    borderRadius: 5,
-  },
-  todoContainer: {
-    marginTop: 30,
-  },
-  todoItem: {
-    height: 50,
-    backgroundColor: "#f1f5f9",
-    borderRadius: 5,
-    padding: 10,
-    paddingHorizontal: 20,
-    justifyContent: "center",
-    marginBottom: 10,
-  },
-  todoText: {
-    fontSize: 18,
-  },
-});
