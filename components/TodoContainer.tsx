52decc86f68eb5098c5479f64348e073773202df
zwelhtetyan <zwelhtetyan2001@gmail.com>
2023-08-07 17:38:35 +0630

    feat(ui): change button position

--- components/TodoContainer.tsx
+++ components/TodoContainer.tsx
@@ -16,26 +16,34 @@ export default function TodoContainer({
 }: Todos) {
   return (
     <>
-      <View style={{ alignItems: "center" }}>
-        <AppButton
-          title="Add Todo"
-          style={{ marginTop: 50 }}
-          onPress={openModal}
-        />
-      </View>
-
       {todos.length > 0 ? (
-        <FlatList
-          data={todos}
-          renderItem={({ item: todo }) => (
-            <TodoItem todo={todo} onDeleteTodo={handleDeleteTodo} />
-          )}
-          style={styles.todoContainer}
-          keyExtractor={(item) => item.id.toString()}
-        />
+        <>
+          <View style={{ alignItems: "center" }}>
+            <AppButton
+              title="Add Todo"
+              style={{ marginTop: 50 }}
+              onPress={openModal}
+            />
+          </View>
+          <FlatList
+            data={todos}
+            renderItem={({ item: todo }) => (
+              <TodoItem todo={todo} onDeleteTodo={handleDeleteTodo} />
+            )}
+            style={styles.todoContainer}
+            keyExtractor={(item) => item.id.toString()}
+          />
+        </>
       ) : (
         <View style={styles.noTodoTextContainer}>
           <Text style={styles.noTodoText}>Add your very first Todo 📝🤩</Text>
+          <View style={{ alignItems: "center" }}>
+            <AppButton
+              title="Add Todo"
+              style={{ marginTop: 20 }}
+              onPress={openModal}
+            />
+          </View>
         </View>
       )}
     </>
@@ -47,7 +55,7 @@ const styles = StyleSheet.create({
     marginTop: 30,
   },
   noTodoTextContainer: {
-    marginTop: 30,
+    marginTop: 50,
     alignItems: "center",
   },
   noTodoText: {
